1f8d0e87a7d992b9ae6432d713cb0b767001d0e0
Eric <leleueri@users.noreply.github.com>
2016-08-25 09:27:43 +0200

    fix syntax error

--- argos-agent/src/main/bin/argos-agent.sh
+++ argos-agent/src/main/bin/argos-agent.sh
@@ -23,7 +23,7 @@ case "$1" in
                 exit 1
             fi
         fi
-        nohup $JAVA -cp "$ARGOS_CLASSPATH:$CLASSPATH"i $ARGOS_JAVA_OPTS io.argos.agent.Launcher > $argos_log_file 2>&1 &
+        nohup $JAVA -cp "$ARGOS_CLASSPATH:$CLASSPATH" $ARGOS_JAVA_OPTS io.argos.agent.Launcher > $argos_log_file 2>&1 &
         retval=$?
         pid=$!
         [ $retval -eq 0 ] && echo "$pid" > $argos_pid_file
